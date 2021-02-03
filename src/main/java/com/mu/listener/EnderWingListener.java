8415d26895ea934e7c7f8fe42291bcbbd2f31160
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 15:54:17 -0500

    Create EnderWingListener.java
    Ender Wing Stuff

---
+++ src/main/java/com/mu/listener/EnderWingListener.java
@@ -0,0 +1,20 @@
+package com.mu.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+public class EnderWingListener implements Listener {
+
+@EventHandler
+
+    public void onFly(PlayerToggleFlightEvent e){
+    Player p = e.getPlayer();
+    
+
+
+}
+
+
+}
